17e9acb5065df029deafc805227df1a61082b74e
emiresen <emiresen246@gmail.com>
2024-08-23 16:57:31 +0300

    Dockerfile and nginx.conf added for google k8 deploy

--- src/store/feature/config.ts
+++ src/store/feature/config.ts
@@ -1,6 +1,8 @@
-const SERVER_LOCAL = 'http://localhost:9090'
+// const SERVER_CLUSTER = 'http://deployment-tennisspring:9090'
+const SERVER_LOADBALANCER = 'http://35.224.211.48:9090'
+// const SERVER_LOCAL = 'http://localhost:9090'
 
-const server = SERVER_LOCAL;
+const server = SERVER_LOADBALANCER;
 const config = {
     BASE_URL: server
 };
