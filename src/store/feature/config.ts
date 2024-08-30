8a1909611a9a77d7bcd36b16e7fa4c10d423778c
emiresen <emiresen246@gmail.com>
2024-08-30 19:49:28 +0300

    config.ts corrected for new GKE ips

--- src/store/feature/config.ts
+++ src/store/feature/config.ts
@@ -1,8 +1,8 @@
-// const SERVER_CLUSTER = 'http://deployment-tennisspring:9090'
-const SERVER_LOADBALANCER = 'https://35.224.211.48:9090'
+const SERVER_CLUSTER = 'http://server-cluster-ip-service:9090'
+// const SERVER_LOADBALANCER = 'https://35.224.211.48:9090'
 // const SERVER_LOCAL = 'http://localhost:9090'
 
-const server = SERVER_LOADBALANCER;
+const server = SERVER_CLUSTER;
 const config = {
     BASE_URL: server
 };
